cdfd9d87698703595f0e302f5c46c297ae7aebcb
300983741 <300983741@PR-E216-66>
2019-08-12 16:05:49 -0400

    completed initial UI for Lab08

--- COMP123-S2019-Lesson8/Lab08Form.cs
+++ COMP123-S2019-Lesson8/Lab08Form.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace COMP123_S2019_Lesson8
 {
-    public partial class Form1 : Form
+    public partial class Lab08 : Form
     {
-        public Form1()
+        public Lab08()
         {
             InitializeComponent();
         }
